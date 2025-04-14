d435744f58aa220c7fd8e14e1afcfdee1f4bcac6
MatteoDinville <matteo.dinville@ynov.com>
2025-04-14 23:04:29 +0200

    FT_01_Authentication - implemented tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
 import { router } from './pages/routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 router.load().then(() => {
 	ReactDOM.createRoot(document.getElementById('root')!).render(
 		<React.StrictMode>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</React.StrictMode>,
 	)
 })
