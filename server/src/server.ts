c7425e5bfefa397e72fdaeaada858b9714704b04
MatteoDinville <matteo.dinville@ynov.com>
2025-05-15 16:46:45 +0200

    FT_01_Authentication - Server - update server side

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
+import 'express-async-errors'
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import routes from './routes';
+import config from './config';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? 4000;
+const port = config.port;
 
 app.use(cors({
 	origin: 'http://localhost:5173',
@@ -17,15 +19,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", routes)
-
-app.get('/api/home', (req, res) => {
-	try {
-		res.json({ message: 'Bienvenue sur la page d\'accueil' });
-	} catch (error) {
-		console.error('Home page error:', error);
-		res.status(500).json({ message: 'Une erreur est survenue' });
-	}
-});
 
 app.listen(port, () => {
 	console.log(`🚀 Server is running on http://localhost:${port}`);
