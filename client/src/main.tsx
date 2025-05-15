90f53e1b0b3aa689ef31745dadcac1a092476abf
MatteoDinville <matteo.dinville@ynov.com>
2025-05-15 16:45:23 +0200

    FT_01_Authentication - Client - update client side

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-import App from './App'
-import { router } from './pages/routes'
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +12,14 @@ router.load().then(() => {
 	ReactDOM.createRoot(document.getElementById('root')!).render(
 		<React.StrictMode>
 			<QueryClientProvider client={queryClient}>
-				<App />
+				<RouterProvider router={router} />
+				<Toaster
+					position="top-center"
+					toastOptions={{
+						style: { background: '#363636', color: '#fff' },
+						success: { duration: 3000 },
+					}}
+				/>
 			</QueryClientProvider>
 		</React.StrictMode>,
 	)
